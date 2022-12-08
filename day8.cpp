6d2238ef7b2d7c5df08250068af6a14d44030c2f
VeressIris <irisveress@gmail.com>
2022-12-08 20:44:15 +0200

    day8 full solution

--- day8.cpp
+++ day8.cpp
@@ -4,9 +4,11 @@
 
 using namespace std;
 
-ifstream fin ("day6.in");
+ifstream fin ("day8.in");
 
-void generateTreeMatrix(int trees[100][100], int &row)
+#define SIZE 99
+
+void generateTrees(int trees[SIZE][SIZE], int &n, int &m)
 {
     string input;
     int len;
@@ -15,56 +17,168 @@ void generateTreeMatrix(int trees[100][100], int &row)
         len = input.length();
         for (int j = 0; j < len; j++)
         {
-            trees[row][j] = input[j] - '0';
+            trees[n][j] = input[j] - '0';
         }
 
-        row++;
+        n++;
     }
+
+    m = len;
 }
 
-bool visible(int ii, int jj, int row, int trees[100][100])
+int part1(int row, int col, int trees[SIZE][SIZE])
 {
-    if (ii == 0 || jj == 0 || ii == row - 1 || jj == row - 1)
-    {
-        return true;
-    }
+    int ans = 0;
+    int treeHeight;
+    bool visible;
 
-    //check all other trees around current tree
-    int treeHeight = trees[ii][jj];
-    for (int i = ii; i >= 0; i--)
+    for (int i = 0; i < row; i++)
     {
-        for (int j = jj; j >= 0; j--)
+        for (int j = 0; j < col; j++)
         {
-            if (treeHeight > trees[ii][jj - 1] || treeHeight > trees[ii][jj + 1] ||
-                treeHeight > trees[ii - 1][jj] || treeHeight > trees[ii + 1][jj])
+            treeHeight = trees[i][j];
+
+            //check left
+            for (int jj = j - 1; jj >= 0; jj--)
+            {
+                if (treeHeight <= trees[i][jj])
+                {
+                    visible = false;
+                    break;
+                }
+            }
+            if (visible)
+            {
+                ans++;
+                continue;
+            }
+
+            //check right
+            for (int jj = j + 1; jj < col; jj++)
+            {
+                if (treeHeight <= trees[i][jj])
+                {
+                    visible = false;
+                    break;
+                }
+            }
+            if (visible)
+            {
+                ans++;
+                continue;
+            }
+
+            //check above
+            for (int ii = i - 1; ii >= 0; ii--)
+            {
+                if (treeHeight <= trees[ii][j])
+                {
+                    visible = false;
+                    break;
+                }
+            }
+            if (visible)
+            {
+                ans++;
+                continue;
+            }
+
+            //check below
+            for (int ii = i + 1; ii < row; ii++)
+            {
+                if (treeHeight <= trees[ii][j])
+                {
+                    visible = false;
+                    break;
+                }
+            }
+            if (visible)
             {
                 ans++;
             }
         }
     }
 
-    return false;
+    return ans;
+}
+
+int part2(int row, int col, int trees[SIZE][SIZE])
+{
+    int ans = 0;
+    int treeHeight;
+    int score = 1;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            treeHeight = trees[i][j];
+            score = 1;
+            int directionScore[4] = {0};
+
+            //check left
+            for (int jj = j - 1; jj >= 0; jj--)
+            {
+                directionScore[0]++;
+                if (treeHeight <= trees[i][jj])
+                {
+                    break;
+                }
+            }
+            
+            //check right
+            for (int jj = j + 1; jj < col; jj++)
+            {
+                directionScore[1]++;
+                if (treeHeight <= trees[i][jj])
+                {
+                    break;
+                }
+            }
+            
+            //check above
+            for (int ii = i - 1; ii >= 0; ii--)
+            {
+                directionScore[2]++;
+                if (treeHeight <= trees[ii][j])
+                {
+                    break;
+                }
+            }
+            
+            //check below
+            for (int ii = i + 1; ii < row; ii++)
+            {
+                directionScore[3]++;
+                if (treeHeight <= trees[ii][j])
+                {
+                    break;
+                }
+            }
+            
+            for (auto x : directionScore)
+            {
+                score *= x;   
+            }
+
+            if (score > ans)
+            {
+                ans = score;
+            }
+        }
+    }
+
+    return ans;
 }
 
 int main()
 {
-    int trees[100][100];
+    int trees[SIZE][SIZE];
     int row = 0;
-    generateTreeMatrix(trees, row);
-
-    int ans = 0;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < row; j++)
-        {
-            if (visible(i, j, row, trees))
-            {
-                ans++;
-            }
-        }
-    }
-
-    cout << ans;
-
+    int col = 0;
+    generateTrees(trees, row, col);
+    
+    //cout << part1(row, col, trees);
+    cout << part2(row, col, trees);
+    
     return 0;
 }
